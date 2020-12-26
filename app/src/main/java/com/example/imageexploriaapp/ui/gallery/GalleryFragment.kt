2cf351334eecf6a2d1681491f80403a12e8566bb
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2020-12-26 21:18:05 +0530

    implemented pagination

--- app/src/main/java/com/example/imageexploriaapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imageexploriaapp/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,22 @@
 package com.example.imageexploriaapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.imageexploriaapp.R
 import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
-@EntryPoint
+@AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+    private val viewModel by viewModels<GalleryViewModel> ()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner){
+
+        }
+    }
 }
