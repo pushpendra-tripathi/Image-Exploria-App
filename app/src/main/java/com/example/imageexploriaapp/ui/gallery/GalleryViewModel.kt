6a9cff9f10f6276ceed1741d669432c118470f3b
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2020-12-27 19:12:04 +0530

    default query changed

--- app/src/main/java/com/example/imageexploriaapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/imageexploriaapp/ui/gallery/GalleryViewModel.kt
@@ -21,6 +21,6 @@ class GalleryViewModel @ViewModelInject constructor(
     }
 
     companion object {
-        private const val DEFAULT_QUERY = "cats"
+        private const val DEFAULT_QUERY = "cars"
     }
 }
